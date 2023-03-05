10a46963853546441281a1a4946518056241edaf
Sinan Demir <snndmrprx@gmail.com>
2023-03-05 15:28:57 +0300

    Duende Identity Server implemented.

---
+++ Restaurant.Services.Identity/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Restaurant.Services.Identity.Models;
+
+namespace Restaurant.Services.Identity.DbContexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
---
+++ Restaurant.Services.Identity/Models/ApplicationUser.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Restaurant.Services.Identity.Models
+{
+    public class ApplicationUser : IdentityUser { }
+}
